3c47d3a664a354bb001e7a0eabbf2f0361ae7df8
hienonichan <hvu6582@gmail.com>
2024-03-10 21:54:34 +0700

    define DelayState

--- projectgameSDL/DelayState.cpp
+++ projectgameSDL/DelayState.cpp
@@ -4,27 +4,36 @@
 #include"GameControl.h"
 #include"HomeState.h"
 #include"ObjectTextureManager.h"
+#include"GameButton.h"
 
 void DelayState::update() {
-
+	for (int i = 0; i < gameObjects.size(); i++) {
+		gameObjects[i]->update();
+	}
 }
 
 void DelayState::render() {
-
+	for (int i = 0; i < gameObjects.size(); i++) {
+		gameObjects[i]->draw();
+	}
 }
 
 bool DelayState::loadState() {
-
+	ObjectTextureManager::getInstance()->loadTexture("C:/projectgameSDL/projectgameSDL/continue.png", "continue", GameControl::getInstance()->getRenderer());
+	ObjectTextureManager::getInstance()->loadTexture("C:/projectgameSDL/projectgameSDL/menu.png", "menu", GameControl::getInstance()->getRenderer());
+	GameObject* button1 = new GameButton("continue", 200, 200, 135, 58, 1, turnToPlay);
+	GameObject* button2 = new GameButton("menu", 200, 260, 135, 57, 1, turnToHome);
+	gameObjects.push_back(button1);
+	gameObjects.push_back(button2);
 	std::cout << "loading delaySTate\n";
 	return true;
 }
 
 bool DelayState::exitState() {
-
+	
 	std::cout << "exiting delayState\n";
 	return true;
 }
-
 
 
 void DelayState::turnToHome() {
